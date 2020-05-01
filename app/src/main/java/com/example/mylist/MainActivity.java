e510d7a29981b57f2c289494c08203cdd2dde078
estherVS <esther.vieirabv@gmail.com>
2020-05-01 18:16:48 -0400

    first commit

---
+++ app/src/main/java/com/example/mylist/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.mylist;
+
+import android.os.Bundle;
+
+import com.example.mylist.adapter.ContactsAdapter;
+import com.example.mylist.model.Contact;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView mListContacts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mListContacts = findViewById(R.id.lvContacts);
+
+        List<Contact> contactList = new ArrayList<Contact>();
+        contactList.add(new Contact("Esther", "Hey", "11:45AM", null));
+        contactList.add(new Contact("Sasuke", "amateresu", "00:45PM", null));
+        contactList.add(new Contact("Hinata", "Shoyo", "11:45AM", null));
+        contactList.add(new Contact("Suga", "hi", "07:35AM", null));
+
+        ContactsAdapter adapter = new ContactsAdapter(contactList, this);
+
+        mListContacts.setAdapter(adapter);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/mylist/adapter/ContactsAdapter.java
@@ -0,0 +1,59 @@
+package com.example.mylist.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.mylist.R;
+import com.example.mylist.model.Contact;
+
+import java.util.List;
+
+public class ContactsAdapter extends BaseAdapter {
+
+    List<Contact> contactList;
+    Context context;
+
+    public ContactsAdapter(List<Contact> contactList, Context context) {
+        this.contactList = contactList;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return contactList.size();
+    }
+
+    @Override
+    public Contact getItem(int position) {
+        return contactList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.contact_item_list, parent, false);
+
+       TextView tvName = view.findViewById(R.id.tv_nome);
+       TextView tvDescription = view.findViewById(R.id.tv_description);
+       TextView tvLastSeen = view.findViewById(R.id.tvLastSeen);
+       ImageView photo = view.findViewById(R.id.iv_photo);
+
+      Contact contact = contactList.get(position);
+      tvName.setText(contact.getName());
+      tvDescription.setText(contact.getDescription());
+      tvLastSeen.setText(contact.getLastSeen());
+
+        return view;
+    }
+}
